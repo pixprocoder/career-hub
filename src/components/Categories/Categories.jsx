8a1e3d54ba6ec8fca462fcc4543fb177ac184efd
skpixels <makkobir79@gmail.com>
2023-06-24 14:21:44 +0300

    feature jobs section completed

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -6,13 +6,14 @@ const Categories = () => {
   return (
     <section className="py-12 my-32">
       <div className="container mx-auto px-4">
-        <div className="text-center">
-          <h2 className="text-3xl lg:text-4xl text-gray-800 font-bold mb-4">
-            Next Section Headline
+        <div className="text-center mb-5">
+          <h2 className="text-3xl lg:text-6xl text-blue-400 font-bold mb-4">
+            Feature Jobs
           </h2>
-          <p className="text-lg text-gray-700 mb-8">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-            commodo ligula eget dolor.
+          <p className="text-lg  px-56 text-gray-700 font-light mb-8">
+            This is the list of all the jobs that are featured on our website.
+            You can apply for these jobs and get hired. are featured on our
+            website. You can apply for these jobs and get hired.
           </p>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
